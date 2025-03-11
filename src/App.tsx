5b8026476298fe59bd3ab711358462a710b45d02
David Perez <perez.david3001@gmail.com>
2025-03-10 21:49:10 -0500

    Update next-themes to version 0.4.5, implement ThemeProvider and Layout components. Set theme to dark.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import { BrowserRouter } from "react-router-dom";
 import "./App.css";
+import Layout from "./components/layout";
+import { ThemeProvider } from "./context/theme-provider";
 
 function App() {
-  return <BrowserRouter></BrowserRouter>;
+  return (
+    <BrowserRouter>
+      <ThemeProvider defaultTheme="dark">
+        <Layout>Hello</Layout>
+      </ThemeProvider>
+    </BrowserRouter>
+  );
 }
 
 export default App;
